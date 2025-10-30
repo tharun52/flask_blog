d66861b2dfbe867009852b89cac9cc1d6cd27546
tharun <88714069+tharun52@users.noreply.github.com>
2025-10-30 17:01:03 +0530

    changed db name

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_db_connection():
         host=os.getenv('DB_ENDPOINT'),
         user=os.getenv('DB_USERNAME'),
         password=os.getenv('DB_PASSWORD'),
-        database='appdb',
+        database='database-1',
         cursorclass=pymysql.cursors.DictCursor
     )
 
